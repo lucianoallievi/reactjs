9bf2922d4393659ac558d842455d41af5b153af8
Luciano Allievi <lucianoallievi@gmail.com>
2023-04-19 20:16:53 -0300

    primer commit

---
+++ src/components/ItemListContainer.jsx
@@ -0,0 +1,9 @@
+export const ItemListContainer = ({ saludo }) => {
+  return (
+    <div className="list__container">
+      <h2>Item List Container</h2>
+      <hr />
+      <p>{saludo}</p>
+    </div>
+  );
+};
